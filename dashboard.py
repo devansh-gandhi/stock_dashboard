fec418cc58100e9cd0c2f63e1cd991593ee1edc0
Devansh Gandhi <dgandhi4@illinois.edu>
2019-06-24 12:59:48 -0500

    css added

--- dashboard.py
+++ dashboard.py
@@ -6,7 +6,6 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
-#from app import app
 from dash.dependencies import Input, Output
 
 from elasticsearch import Elasticsearch
@@ -24,38 +23,32 @@ else:
 
 app.layout = html.Div([
 
-    html.H1("Stock Prices", style={'textAlign': 'center', 'margin':'0px',}),
+    html.H1("Stock Prices", style={'textAlign': 'center', 'margin': '0px', }),
 
     html.Div([
         dcc.Dropdown(id='my-dropdown',
-                options=[{'label': 'Microsoft', 'value': 'MSFT'},{'label': 'Apple', 'value': 'AAPL'},{'label': 'Google', 'value': 'GOOG'}],
-                value='MSFT',
-                style={"display": "block","margin-left": "auto","margin-right": "auto", }),
+                options=[{'label': 'Microsoft', 'value': 'MSFT'}, {'label': 'Apple', 'value': 'AAPL'},
+                         {'label': 'Google', 'value': 'GOOG'}], value='MSFT',),
+        ], id='my-dropdown-div'),
 
-
-
-    ], style={ 'width':'40%', 'margin':'0px auto', } ),
-
-
-
-    dcc.Graph(id='my-graph', style={'textAlign': 'center', 'align': 'center', 'width':'70%'  ,'margin': '0px auto', 'background-color': 'black' }, config={'displayModeBar': False},   ),
+    dcc.Graph(id='my-graph', config={'displayModeBar': False},),
 
 
     html.Table([
             html.Tr([
-                html.Td(html.H3('News Analysis')  , style={'width':'50%'} ),
-                html.Td(html.H3('Twitter feed Analysis') , style={'width':'50%'}),
+                html.Td(html.H3('News Analysis'), style={'width': '50%'}),
+                html.Td(html.H3('Twitter feed Analysis'), style={'width': '50%'}),
             ]),
 
             html.Tr([
                 html.Td(id='news_table'),
 
-            ], style={} ),
+            ],),
 
 
         ],
-        style={'textAlign': 'center', 'align': 'center', 'width':'70%', 'margin': '0px auto' }
-    ) ,
+        style={'textAlign': 'center', 'align': 'center', 'width': '90%', 'margin': '0px auto'}
+    ),
 
     ], className="container")
 
@@ -63,7 +56,7 @@ app.layout = html.Div([
 @app.callback(Output('my-graph', 'figure'),
               [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
-    dropdown = {"MSFT": "Microsoft","AAPL": "Apple","GOOG": "Google",}
+    dropdown = {"MSFT": "Microsoft", "AAPL": "Apple", "GOOG": "Google", }
     trace1 = []
     trace2 = []
 
@@ -73,10 +66,10 @@ def update_graph(selected_dropdown_value):
 
     stock_data_df['timestamp'] = pd.to_datetime(stock_data_df.timestamp, infer_datetime_format=True)
 
-    trace1.append(go.Scatter(x=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["timestamp"],y=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["open"],mode='lines',
-        opacity=0.7,name=f'Open {dropdown[selected_dropdown_value]}',textposition='bottom center'))
-    trace2.append(go.Scatter(x=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["timestamp"],y=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["close"],mode='lines',
-        opacity=0.6,name=f'Close {dropdown[selected_dropdown_value]}',textposition='bottom center'))
+    trace1.append(go.Scatter(x=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["timestamp"], y=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["open"],mode='lines',
+        opacity=0.7, name=f'Open {dropdown[selected_dropdown_value]}', textposition='bottom center'))
+    trace2.append(go.Scatter(x=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["timestamp"], y=stock_data_df[stock_data_df["stock-symbol"] == selected_dropdown_value]["close"],mode='lines',
+        opacity=0.6 , name=f'Close {dropdown[selected_dropdown_value]}',textposition='bottom center'))
 
 
 
@@ -102,22 +95,12 @@ def update_news_feed(selected_dropdown_value):
     #print(news_data_df.head() )
     return html.Table(
 
+        [html.Tr([html.Td(html.Th('Title'),), html.Td(html.Th('Sentiment'), style={'width': '27%', })],
+            style={'width': '100%', 'text-align': 'center', })] +
 
-        [html.Tr([html.Td(html.Th('Description') , style={  'colspan': '5'  }),
-            html.Td(html.Th('Sentiment') , style={'width':'27%', })],
-            style={'width': '100%','text-align':'center', })] +
+        [html.Tr([html.Td([news_data_df.loc[i]['title']], ), html.Td([news_data_df.loc[i]['sentiment']], style={'width': '30%',})], style={'width': '100%',}) for i in range(len(news_data_df))],
 
-
-        [html.Tr([html.Td([news_data_df.loc[i]['title']], ) , html.Td([news_data_df.loc[i]['sentiment']], style={'width':'30%',})], style={'width':'100%',}) for i in range(len(news_data_df))  ],
-
-        style={ 'width': '100%', 'display':'block', 'text-align':'left' ,},
-
-
-
-    )
-
-
-
+    style={'width': '100%', 'display': 'block', 'text-align': 'left', }, id='news_table_data')
 
 if __name__ == '__main__':
     app.run_server(debug=False)
